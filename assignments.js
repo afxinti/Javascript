f09de71069cee2dc2433c720320756d7ed86c04a
afroditi chintipatoglou <afxintip@hotmail.com>
2022-06-29 00:19:23 +0300

    two new excercises

--- assignments.js
+++ assignments.js
@@ -1,5 +1,23 @@
 //excercise 1
-let country = "Greece";
-let continent = "Europe";
-let population = "10678632";
-console.log(country,continent,population);
+const country = "Greece";
+const continent = "Europe";
+let population = 11;
+console.log(country,continent,population); 
+
+//excercise 2
+const isIsland = false;
+const language = "greek"
+console.log(typeof isIsland);
+
+//excercise 3
+const halfPopulation = population/2;
+population ++;
+console.log('we found one extra milion: ',population);
+console.log(population > 6);
+console.log(population < 33); 
+let description = 
+    country +
+    " is in " + continent + 
+    ", and its " + population + 
+    " million people speak " + language;
+console.log (halfPopulation, description);
---
+++ script.js
@@ -0,0 +1,10 @@
+//OPERATORS
+
+//Assignments operations
+let x= 2**3; // 2 sti 3i ara 2*2*2 =8
+x += 10; // auto einai x=x + 10
+x *= 5; // dld x = x * 5
+x++; // x+1
+x--; //x-1
+
+//comparison operators
