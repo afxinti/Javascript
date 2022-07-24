6b1e1eb91294e9cf01753bdffc2d75fb60d29419
afroditi chintipatoglou <afxintip@hotmail.com>
2022-07-24 22:22:20 +0300

    new assignments

--- assignments.js
+++ assignments.js
@@ -15,9 +15,17 @@ population ++;
 console.log('we found one extra milion: ',population);
 console.log(population > 6);
 console.log(population < 33); 
-let description = 
-    country +
-    " is in " + continent + 
-    ", and its " + population + 
-    " million people speak " + language;
+let description = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
 console.log (halfPopulation, description);
+
+//excercise 4 stiring literals
+description = `The country ${country} is as big as ${population} million people`;
+
+//excercise 5
+if (population>33) {
+    console.log(`${country}'s population is above average`);
+}else {
+    console.log(`${country}'s population is ${33-population} million bellow average`);
+};
+//excercise 6
+
--- script.js
+++ script.js
@@ -7,4 +7,31 @@ x *= 5; // dld x = x * 5
 x++; // x+1
 x--; //x-1
 
-//comparison operators
+// to write big string with var and other stuff use backstick `` above tab
+
+//coding challenge 1
+
+//testdata1
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+/*testdata2
+const massMark = 95;
+const heightMark = 1.88;
+const massJohn = 85;
+const heightJohn = 1.76;*/
+
+//calculate BMIs and compare them
+const markBMI = massMark/ heightMark**2;
+const johnBMI = massJohn / heightJohn**2;
+const markHigherBMI = markBMI > johnBMI;
+console.log (markBMI,johnBMI,markHigherBMI); 
+
+//coding challenge 2
+ if (markHigherBMI) {
+  console.log(`Mark's BMI(${markBMI} is higher than John's (${johnBMI}!)`);  
+ }else {
+  console.log(`John's BMI(${johnBMI} is higher than Mark's (${markBMI}!)`);  
+ };
