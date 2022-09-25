a10b34b19a35006863eb757ee9dc2ac1afadab01
afroditi chintipatoglou <afxintip@hotmail.com>
2022-09-25 17:04:58 +0300

    highscore_replay buttons

--- projectFindNumber/script.js
+++ projectFindNumber/script.js
@@ -1,20 +1,41 @@
 'use strict';
 
-let winningNumber = Math.trunc(Math.random() * 30 + 1);
+// calculates random number
+function calcLuckyNumber() {
+  let a = Math.trunc(Math.random() * 30 + 1);
+  return a;
+}
 
-document.querySelector('.number').textContent = winningNumber;
-
+let winningNumber = calcLuckyNumber();
 let message = document.querySelector('.distance').textContent;
-const check = document.querySelector('.check');
 let guess = Number(document.querySelector('.guess').value);
+const check = document.querySelector('.check');
+const replay = document.querySelector('.replay');
+let score = 20;
+let highscore = 0;
+//check the lucky guess of player and give feedback
 check.addEventListener('click', function () {
   guess = Number(document.querySelector('.guess').value);
   if (guess === winningNumber) {
-    message = 'Well played you found it!';
+    document.querySelector('.number').textContent = winningNumber;
+    message = 'Well played, you found it!';
+    score = score;
   } else if (guess > winningNumber) {
     message = 'Too high';
+    score = score - 1;
   } else {
     message = 'Too low';
+    score = score - 1;
   }
   document.querySelector('.distance').textContent = message;
+  document.querySelector('.score').textContent = +score;
+});
+
+replay.addEventListener('click', function () {
+  winningNumber = calcLuckyNumber();
+  if (highscore < score) {
+    highscore = score;
+  }
+  score = 20;
+  document.querySelector('.highscore').textContent = +highscore;
 });
