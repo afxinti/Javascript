ff76a771ca3c95661299039e20d7a64d2d07d46a
Afroditi Xinti <102307107+afxinti@users.noreply.github.com>
2022-11-17 00:36:41 +0200

    function for gradient change and printing value

--- projectBackgroundGenerator/script.js
+++ projectBackgroundGenerator/script.js
@@ -1,12 +1,15 @@
-const colors = document.querySelector('h2').innerText;
+let colors = document.querySelector('.colors');
 let color1 = document.querySelector('.color1');
 let color2 = document.querySelector('.color2');
 let body = document.getElementById('back');
+// console.log(body,color1,color2)
 
 function changeBackground() {
+  console.log(color1,color2)
   body.style.background = `linear-gradient(to right, ${color1.value},${color2.value} )`;
+  colors.textContent = `Right: ${color1.value}, Left: ${color2.value}`;
 }
 
-color1.addEventListener('input', changeBackground());
+color1.addEventListener('input', changeBackground);
 
-color2.addEventListener('input', changeBackground());
+color2.addEventListener('input', changeBackground);
